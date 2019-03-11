847ed2c4fcb6ca7dbcfef187457286b4d72d1ab9
Dee <fantp6@gmail.com>
2019-03-11 23:36:43 +0800

    update send_ttn

--- send_ttn.py
+++ send_ttn.py
@@ -18,47 +18,47 @@ class LoRaWANsend(LoRa):
         self.nwkey = nwkey
         self.appkey = appkey
         
-    def switch_to_tx(self):
-        self.set_mode(MODE.SLEEP)
-        self.set_dio_mapping([1,0,0,0,0,0])
-        
-    def switch_to_rx(self):
-        self.set_mode(MODE.SLEEP)
-        self.set_dio_mapping([0,0,0,0,0,0])
-        self.reset_ptr_rx()
-        self.set_mode(MODE.RXSINGLE)
-        
     def on_tx_done(self):
+        # 紀錄送出的時間
         global TX_TIMESTAMP
         TX_TIMESTAMP = datetime.datetime.now().timestamp()
         print("TxDone\n")
+        # 切換到rx
         self.set_mode(MODE.STDBY)
         self.clear_irq_flags(TxDone=1)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0,0,0,0,0,0])
+        self.set_invert_iq(1)
+        self.reset_ptr_rx()
         sleep(1)
-        self.switch_to_rx()
+        self.set_mode(MODE.RXSINGLE)
         
     def on_rx_done(self):
         print("RxDone")
-        global LOCK
         self.clear_irq_flags(RxDone=1)
         
-        #read payload
+        # 讀取 payload
         payload = self.read_payload(nocheck=True)
         lorawan = LoRaWAN.new(nwskey, appskey)
         lorawan.read(payload)
         print("get mic: ",lorawan.get_mic())
         print("compute mic: ",lorawan.compute_mic())
         print("valid mic: ",lorawan.valid_mic())
-        print("ACK: ",lorawan.get_mac_payload().get_fhdr().get_fctrl()>>5&0x01)
-        print("direction: ",lorawan.get_direction())
-        print("devaddr: ",''.join(format(x, '02x') for x in lorawan.get_devaddr()))
-        write_config()
+        # 檢查mic
+        if lorawan.valid_mic():
+            print("ACK: ",lorawan.get_mac_payload().get_fhdr().get_fctrl()>>5&0x01)
+            print("direction: ",lorawan.get_direction())
+            print("devaddr: ",''.join(format(x, '02x') for x in lorawan.get_devaddr()))
+            write_config()
+        else:
+            print("Wrong MIC")
         sys.exit(0)
     
     def send(self):
         global fCnt
         lorawan = LoRaWAN.new(nwskey, appskey)
         message = "HELLO WORLD!"
+        # 資料打包,fCnt+1
         lorawan.create(MHDR.CONF_DATA_UP, {'devaddr': devaddr, 'fcnt': fCnt, 'data': list(map(ord, message)) })
         print("fCnt: ",fCnt)
         print("Send Message: ",message)
@@ -69,6 +69,7 @@ class LoRaWANsend(LoRa):
     def time_checking(self):
         global TX_TIMESTAMP
         diff = datetime.datetime.now().timestamp()-TX_TIMESTAMP
+        # 檢查上次傳uplink的時間差
         if diff > 2 :
             print("TIMEOUT!!")
             write_config()
